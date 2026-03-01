727c9b49d32783a9074d8a53a399854b6e368e3e
ystephant <134706137+ystephant@users.noreply.github.com>
2026-03-01 20:20:39 +0100

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,23 @@
+const CACHE = 'horizon-v1';
+
+self.addEventListener('install', e => {
+  self.skipWaiting();
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(['/']))
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
+  );
+});
